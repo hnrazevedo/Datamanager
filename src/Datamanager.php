8b6b9246fe8d439ad569cb202815f712ce7ff285
Henri Azevedo <hnr.azevedo@gmail.com>
2020-08-08 13:26:03 -0300

    Abstração de métodos magicos

--- src/Datamanager.php
+++ src/Datamanager.php
@@ -6,54 +6,17 @@ use HnrAzevedo\Datamanager\DatamanagerException;
 
 abstract class Datamanager
 {
-    use DataTrait, SynchronizeTrait, EntityTrait;
+    use DataTrait, SynchronizeTrait, EntityTrait, MagicsTrait;
 
     protected ?string $table = null;
     protected ?string $primary = null;
     protected array $data = [];
     protected array $where = [''=> ["1",'=',"1"] ];
-    protected array $between = [];
-
-    public function __set(string $prop,$value)
-    {
-        if(is_array($value)){
-            $attr = array_keys($value)[0];
-            $this->data[$prop][$attr] = $value[$attr];
-            return $this;
-        }
-
-        if($this->full){
-            switch($this->data[$prop]['type']){
-                case 'date':
-                    $value = (date_format( date_create_from_format(DATAMANAGER_CONFIG['dateformat'],$value) , 'Y-m-d'));
-                    break;
-            }
-        }
-
-        $this->isSettable($prop);
-
-        $this->data[$prop]['changed'] = true;
-        $this->data[$prop]['value'] = $value;
-        
-        return $this;
-    }
+    protected array $between = [];    
 
     public function getData(): ?array
     {
         return $this->data;
-    }
-
-    public function __get(string $field)
-    {
-        $this->isSettable($field);
-
-        if($this->full){
-            switch($this->data[$field]['type']){
-                case 'date': $this->data[$field]['value'] = (date_format( date_create_from_format('Y-m-d' , $this->data[$field]['value'] ) , DATAMANAGER_CONFIG['dateformat']));
-            }
-        }
-
-        return $this->data[$field]['value'];
     }
 
     public function getCount(): int
