81e39ceb2575989e1612296e06012c96fce36f0a
Henri Azevedo <hnr.azevedo@gmail.com>
2020-08-28 09:03:53 -0300

    corrige exception

--- src/DatamanagerException.php
+++ src/DatamanagerException.php
@@ -8,7 +8,7 @@ class DatamanagerException extends Exception{
     
     public function __construct($message, $code = 0, Exception $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, intval($code), $previous);
     }
 
     public function __toString()
